782a7da44cfcce9ec4cfdf9615f5feabf7eea600
Pedro Gardim <me@pedrogardim.com>
2024-06-11 01:12:56 +0200

    initial astro commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Scriptura",
+      social: {
+        github: "https://github.com/light-stand/scriptura-api",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
